92f4a2ed8200d02dce81b3abf14b4330ac4790b6
alexandru nazare <alexandrunazare@yahoo.it>
2025-06-04 14:06:39 +0200

    added test folder

---
+++ bookkeeper-common/src/test/java/org/apache/bookkeeper/FarlocTest.java
@@ -0,0 +1,12 @@
+package org.apache.bookkeeper.common;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FarlocTest {
+
+    @Test
+    void thisTestAlwaysFails() {
+        //fail("This test is supposed to fail.");
+    }
+}
